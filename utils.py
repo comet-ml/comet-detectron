5fa9bcca69d09e1a71642e6759bb083959cbbe77
DN6 <dhruv.nair@gmail.com>
2021-05-17 12:47:46 +0000

    add function to format model predictions

--- utils.py
+++ utils.py
@@ -6,6 +6,50 @@ import numpy as np
 from detectron2.structures import BoxMode
 
 
+def format_predictions(outputs, annotations):
+    """Format Dectectron Predictions so that they can be visualized using
+    Comet Panels
+
+    Args:
+        outputs (list): List of formatted dicts
+    """
+    data = []
+    prediction = outputs["instances"].to("cpu")
+
+    predicted_boxes = prediction.pred_boxes.tensor.numpy().tolist()
+    predicted_scores = prediction.scores.numpy().tolist()
+    predicted_classes = prediction.pred_classes.numpy().tolist()
+
+    for annotation in annotations:
+        bbox = annotation["bbox"]
+
+        # Convert from numpy.int64 to int
+        x, y, x2, y2 = map(lambda x: x.item(), bbox)
+
+        label = annotation["category_id"]
+        data.append(
+            {
+                "label": f"ground_truth-{label}",
+                "score": 100,
+                "box": {"x": x, "y": y, "x2": x2, "y2": y2},
+            }
+        )
+
+    for predicted_box, predicted_score, predicted_class in zip(
+        predicted_boxes, predicted_scores, predicted_classes
+    ):
+        x, y, x2, y2 = predicted_box
+        data.append(
+            {
+                "label": predicted_class,
+                "box": {"x": x, "y": y, "x2": x2, "y2": y2},
+                "score": predicted_score * 100,
+            }
+        )
+
+    return data
+
+
 def get_balloon_dicts(img_dir):
     json_file = os.path.join(img_dir, "via_region_data.json")
     with open(json_file) as f:

94b95fbcea0c2f922967a5c46778359ad42e7406
DN6 <dhruv.nair@gmail.com>
2021-05-17 12:48:01 +0000

    add eval hooks to training script

--- train.py
+++ train.py
@@ -3,13 +3,16 @@ import comet_ml
 comet_ml.init()
 
 import os
+import random
+import cv2
 
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.engine import DefaultPredictor, hooks
 
 from comet_trainer import CometDefaultTrainer
-from utils import get_balloon_dicts
+from utils import format_predictions, get_balloon_dicts
 
 
 def setup():
@@ -20,7 +23,7 @@ def setup():
         )
     )
     cfg.DATASETS.TRAIN = ("balloon_train",)
-    cfg.DATASETS.TEST = ()
+    cfg.DATASETS.TEST = ("balloon_val",)
     cfg.DATALOADER.NUM_WORKERS = 2
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
         "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
@@ -40,6 +43,31 @@ def setup():
     return cfg
 
 
+def log_predictions(predictor, dataset_dicts, experiment):
+    """Log Model Predictions to Comet for analysis.
+
+    Args:
+        predictor (DefaultPredictor): Predictor Object for Detectron Model
+        dataset_dicts (dict): Dataset Dictionary contaning samples of data and annotations
+        experiment (comet_ml.Experiment): Comet Experiment Object
+    """
+    predictions_data = {}
+    for d in random.sample(dataset_dicts, 3):
+        file_name = str(d["file_name"])
+
+        im = cv2.imread(file_name)
+        annotations = d["annotations"]
+
+        outputs = predictor(
+            im
+        )  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+        formatted_data = format_predictions(outputs, annotations)
+        predictions_data[file_name] = formatted_data
+        experiment.log_image(file_name, name=file_name)
+
+    experiment.log_asset_data(predictions_data, name="predictions-data.json")
+
+
 def main():
     experiment = comet_ml.Experiment()
     cfg = setup()
@@ -54,7 +82,25 @@ def main():
     # Wrap the Detectron Default Trainer
     trainer = CometDefaultTrainer(cfg, experiment)
     trainer.resume_or_load(resume=False)
+
+    # Register Hook to compute metrics using an Evaluator Object
+    trainer.register_hooks(
+        [hooks.EvalHook(10, lambda: trainer.evaluate_metrics(cfg, trainer.model))]
+    )
+
+    # Register Hook to compute eval loss
+    trainer.register_hooks(
+        [hooks.EvalHook(10, lambda: trainer.evaluate_loss(cfg, trainer.model))]
+    )
     trainer.train()
+
+    # Evaluate Model Predictions
+    cfg.MODEL.WEIGHTS = os.path.join(
+        cfg.OUTPUT_DIR, "model_final.pth"
+    )  # path to the model we just trained
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set a custom testing threshold
+    predictor = DefaultPredictor(cfg)
+    log_predictions(predictor, get_balloon_dicts("balloon/val"), experiment)
 
 
 if __name__ == "__main__":
